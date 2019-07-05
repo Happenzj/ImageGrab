17bc995ba161fc29194e854e6c7f287731b1874a
Happenzj <happenzj@163.com>
2019-07-05 10:02:10 +0800

    add a new file

---
+++ src/ImageGrab.cpp
@@ -0,0 +1,108 @@
+#include <fstream>
+#include <boost/timer.hpp>
+ #include <opencv2/imgcodecs.hpp>
+#include <ros/ros.h>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <iostream>
+
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <opencv2/core/core.hpp>
+#include <image_transport/image_transport.h>
+
+/*
+    @Description:
+    This code is used to grap stereo image toipc, and save them in the folder.
+*/
+
+struct ImgSave
+{
+    public:
+        ImgSave(){std::cout<<"Build a new file!"<<std::endl;};
+        void GrabStereo(const sensor_msgs::ImageConstPtr& msgLeft,const sensor_msgs::ImageConstPtr& msgRight);
+        void StereoImageSave(const cv::Mat &ImgLeft, const cv::Mat &ImgRight, const double &timestamp);
+    public:
+        std::ofstream outfile_;  
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "ImageGrab");
+    ros::start();
+    ros::NodeHandle nh;
+
+    ImgSave StereoImgSave;
+    ImgSave *p = &StereoImgSave;
+
+    p->outfile_.open("/home/happenzj/catkin_ws/src/stereoVO-master/data/sequence/timestamp.txt");
+    
+    std::cout<<"Here!!"<<std::endl;
+    if (!p->outfile_) std::cout<<"Open file error!"<<std::endl;
+    
+    message_filters::Subscriber<sensor_msgs::Image> left_sub(nh, "/camera/left/image_raw", 1);
+    message_filters::Subscriber<sensor_msgs::Image> right_sub(nh, "/camera/right/image_raw", 1);
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
+    message_filters::Synchronizer<sync_pol> sync(sync_pol(10), left_sub,right_sub);
+    sync.registerCallback(boost::bind(&ImgSave::GrabStereo,p,_1,_2));
+    
+    ros::spin();
+    ros::shutdown();
+    p->outfile_.close();
+
+    return 0;
+}
+
+void ImgSave::GrabStereo(const sensor_msgs::ImageConstPtr& msgLeft,const sensor_msgs::ImageConstPtr& msgRight)
+{
+    cv_bridge::CvImageConstPtr cv_ptrLeft;
+    try
+    {
+        cv_ptrLeft = cv_bridge::toCvShare(msgLeft);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    cv_bridge::CvImageConstPtr cv_ptrRight;
+    try
+    {
+        cv_ptrRight = cv_bridge::toCvShare(msgRight);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+   StereoImageSave(cv_ptrLeft->image,cv_ptrRight->image,cv_ptrLeft->header.stamp.toSec());
+}
+
+void ImgSave::StereoImageSave(const cv::Mat &ImgLeft, const cv::Mat &ImgRight, const double &timestamp)
+{
+    cv::Mat leftImg = ImgLeft;
+    cv::Mat rightImg = ImgRight;
+    double SaveTimeStamp;
+    if ( leftImg.data==nullptr || rightImg.data==nullptr)
+    std::cout<<"**********Image Errors!**********"<<std::endl;
+    
+    cv::cvtColor(leftImg,leftImg,CV_RGBA2GRAY);
+    cv::cvtColor(rightImg,rightImg,CV_RGBA2GRAY);//zed的图片是4通道RGBA
+
+    cv::resize(leftImg,leftImg,cv::Size(640,360),0,0);
+    cv::resize(rightImg,rightImg,cv::Size(640,360),0,0);
+
+    std::cout<<"ImageSize: "<<rightImg.size()<<std::endl;
+     
+    SaveTimeStamp = timestamp;
+    std::cout<<"savetime: "<<SaveTimeStamp<<std::endl;
+    std::string ImgName  = std::to_string(SaveTimeStamp);
+    
+    outfile_<<ImgName<<"\n";
+    cv::imwrite("/home/happenzj/catkin_ws/src/stereoVO-master/data/sequence/image_0/"+ImgName+".png",leftImg);
+    cv::imwrite("/home/happenzj/catkin_ws/src/stereoVO-master/data/sequence/image_1/"+ImgName+".png",rightImg);
+}
